7469c42c0ab461ca40b56aa1fa7ac7e27916f177
inaimathi <leo.zovic@gmail.com>
2023-04-10 00:24:06 -0400

    Useless back :shrug:

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const back = require('androidjs').back;
 
-back.on("hello from front", function(){
-  back.send("hello from back", "Hello from Android JS backend");
-  back.send("front-log", "Backend log test");
-});
+// back.on("hello from front", function(){
+//   back.send("hello from back", "Hello from Android JS backend");
+//   back.send("front-log", "Backend log test");
+// });
